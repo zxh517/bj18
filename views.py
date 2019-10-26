ef89d37de3396934da6c452a711b51cc77fc84bc
happy <zxh517@126.com>
2019-10-26 11:46:17 +0800

    '创建视图'

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
 	return HttpResponse('index')
+	
+def login(request):
+	return redirect('/index')
+	
